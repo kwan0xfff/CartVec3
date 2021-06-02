304e6da6eb3e3c710dccfbb432db4e185073a991
R. J. Dev. Kwan <6440263+kwan0xfff@users.noreply.github.com>
2021-06-01 20:00:16 -0700

    Cart3d class and test additions.
    Class:
    * Add __init__ using another Cart3d object.
    * Add __mul__ (Cart3d * float, Cart3d * Cart3d), __truediv__ (Cart3d / float).
    * Add __imul__ (Cart3d =* float), __itruediv__ (Cart3d /= float).
    Tests:
    * Add test casesfor __init__, __ne__, __sub__, __mul__, __truediv__,
      __iadd__, __isub__, __imul__, __itruediv__.

--- cartvec3/cart3d.py
+++ cartvec3/cart3d.py
@@ -1,5 +1,6 @@
 """
-cart3d -- Cartesian coordinates, 3 axes, double-precision
+Module cart3d -- Cartesian coordinates, 3 axes, double-precision.
+Includes class Cart3d.
 """
 
 import array as ar
@@ -13,10 +14,19 @@ class Cart3d():
     def __init__(self, *args) -> None:
             acnt = len(args)    # argument count
             if acnt == 0:
+                # no args, fill with zeros
                 self.elems = ar.array('d', Cart3d.zeros)
-            elif acnt == 1 and isinstance(args[0], list) and len(args[0]) == 3:
-                self.elems = ar.array('d', args[0])
+            elif acnt == 1:
+                if isinstance(args[0], list) and len(args[0]) == 3:
+                    # list comprised of 3 floats
+                    self.elems = ar.array('d', args[0])
+                elif isinstance(args[0], Cart3d):
+                    # Cart3d
+                    self.elems = args[0].elems
+                else:
+                    raise TypeError('Cart3d __init__ unknown argument')
             elif acnt == 3 and isinstance(args[0], float):
+                # 3 floats
                 self.elems = ar.array('d', args)
             else:
                 raise TypeError('Cart3d __init__ unrecognized args count')
@@ -24,27 +34,66 @@ class Cart3d():
     def __repr__(self):
         return Cart3d.prefix+self.elems.__repr__()
 
-    def __add__(self, other) :
+    def __add__(self, other):
+        # Cart3d + Cart3d
         c = Cart3d()
         for i in Cart3d.enumx:
             c.elems[i] = self.elems[i] + other.elems[i]
         return c
 
     def __sub__(self, other) :
+        # Cart3d - Cart3d
         c = Cart3d()
         for i in Cart3d.enumx:
             c.elems[i] = self.elems[i] - other.elems[i]
         return c
 
+    def __mul__(self, other):
+        c = Cart3d()
+        if isinstance(other, float): 
+            # Cart3d * float
+            for i in Cart3d.enumx:
+                c.elems[i] = self.elems[i] * other
+            return c
+        elif isinstance(other, Cart3d): 
+            # Cart3d * Cart3d (dot product)
+            s = 0.0
+            for i in Cart3d.enumx:
+                s += self.elems[i] * other.elems[i]
+            return s
+
+    def __truediv__(self, other):
+        # Cart3d / float
+        c = Cart3d()
+        for i in Cart3d.enumx:
+            c.elems[i] = self.elems[i] / other
+        return c
+
     def __iadd__(self, other):
+        # Cart3d += Cart3d
         for i in Cart3d.enumx:
             self.elems[i] += other.elems[i]
         return self
 
     def __isub__(self, other):
+        # Cart3d -= Cart3d
         for i in Cart3d.enumx:
             self.elems[i] -= other.elems[i]
         return self
+
+    def __imul__(self, other):
+        # Cart3d *= float
+        c = Cart3d()
+        for i in Cart3d.enumx:
+            c.elems[i] = self.elems[i] * other
+        return c
+
+    def __itruediv__(self, other):
+        # Cart3d /= float
+        c = Cart3d()
+        for i in Cart3d.enumx:
+            c.elems[i] = self.elems[i] / other
+        return c
 
     def fromlist(self, ll):
         assert len(ll) == 3
--- tests/test_cart3d.py
+++ tests/test_cart3d.py
@@ -17,6 +17,11 @@ def test_init_3args():
     c3 = Cart3d(6.1, 6.2, 6.3)
     assert c3.elems == ar.array('d', [6.1, 6.2, 6.3])
 
+def test_init_Cart3d():
+    c1 = Cart3d([1.0, 2.0, 3.0])
+    c2 = Cart3d(c1)
+    assert c1.elems == c2.elems
+
 def test_init_2args_raise():
     with pytest.raises(TypeError):
         c2 = Cart3d([1.5, 2.5])
@@ -25,12 +30,60 @@ def test_init_strs_raise():
     with pytest.raises(TypeError):
         c1x = Cart3d(["one", "two", "three"])
 
+def test_elems_ne():
+    c123 = Cart3d([1.0, 2.0, 3.0])
+    c234 = Cart3d([2.0, 3.0, 4.0])
+    assert c123.elems != c234.elems
+
 def test_add():
     c123 = Cart3d([1.0, 2.0, 3.0])
     c111 = Cart3d([1.0, 1.0, 1.0])
-    c234 = Cart3d([2.0, 3.0, 4.0])
     csum = c123 + c111
-    assert csum.elems == c234.elems
+    assert csum.elems == ar.array('d',[2.0, 3.0, 4.0])
+
+def test_sub():
+    c123 = Cart3d([1.0, 2.0, 3.0])
+    c111 = Cart3d([1.0, 1.0, 1.0])
+    cdif = c123 - c111
+    assert cdif.elems == ar.array('d',[0.0, 1.0, 2.0])
+
+def test_mul_c3_sc():
+    c123 = Cart3d([1.0, 2.0, 3.0])
+    cresult = c123 * 2.0
+    assert cresult.elems == ar.array('d',[2.0, 4.0, 6.0])
+
+def test_mul_c3_c3():
+    c123 = Cart3d([1.0, 2.0, 3.0])
+    c222 = Cart3d([2.0, 2.0, 2.0])
+    cresult = c123 * c222
+    assert cresult == 12
+
+def test_truediv():
+    c123 = Cart3d([1.0, 2.0, 3.0])
+    cdiv = c123 / 2.0
+    assert cdiv.elems == ar.array('d',[0.5, 1.0, 1.5])
+
+def test_iadd():
+    csum = Cart3d([1.0, 2.0, 3.0])
+    c111 = Cart3d([1.0, 1.0, 1.0])
+    csum += c111
+    assert csum.elems == ar.array('d',[2.0, 3.0, 4.0])
+
+def test_isub():
+    cdif = Cart3d([1.0, 2.0, 3.0])
+    c111 = Cart3d([1.0, 1.0, 1.0])
+    cdif -= c111
+    assert cdif.elems == ar.array('d',[0.0, 1.0, 2.0])
+
+def test_imul():
+    cccc = Cart3d([1.0, 2.0, 3.0])
+    cccc *= 2.0
+    assert cccc.elems == ar.array('d',[2.0, 4.0, 6.0])
+
+def test_itruediv():
+    cccc = Cart3d([1.0, 2.0, 3.0])
+    cccc /= 2.0
+    assert cccc.elems == ar.array('d',[0.5, 1.0, 1.5])
 
 def test_fromlist():
     c246 = Cart3d()
