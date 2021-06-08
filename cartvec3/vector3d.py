da7160d2a406a2d2a084205db5639fe2db7c3bfb
R. J. Dev. Kwan <6440263+kwan0xfff@users.noreply.github.com>
2021-06-08 08:17:25 -0700

    Add methods to class Vector3d:
    * normalize(), lengthSquared(), length(), angle().
    * also helper normalize() outside the class.

--- cartvec3/vector3d.py
+++ cartvec3/vector3d.py
@@ -5,6 +5,21 @@ Includes class Vector3d
 
 from cart3d import Cart3d
 import array as ar
+import math
+
+def _lenSq(v):
+    "length squared of a vector."
+    s = 0.0
+    for i in Cart3d.enumx:
+        s += v.elems[i] * v.elems[i]
+    return s
+
+def normalize(v):
+    "Normalize a vector to unit length."
+    lsq = _lenSq(v)
+    norm = 1.0 / math.sqrt(lsq)
+    v1 = v * norm
+    return v1
 
 class Vector3d(Cart3d):
     "A 3-element vector of double-precision elements."
@@ -27,3 +42,24 @@ class Vector3d(Cart3d):
         v0.elems[1] = v1.elems[2]*v2.elems[0] - v1.elems[0]*v2.elems[2]
         v0.elems[2] = v1.elems[0]*v2.elems[1] - v1.elems[1]*v2.elems[0]
         return v0
+
+    def normalize(self):
+        "Normalize vector in-place. No return value."
+        lsq = _lenSq(self)
+        norm = 1.0 / math.sqrt(lsq)
+        for i in Cart3d.enumx:
+            self.elems[i] *= norm
+
+    def lengthSquared(self):
+        "Return the squared length of this vector."
+        return _lenSq(self)
+
+    def length(self):
+        "Return the length of this vector."
+        return math.sqrt(_lenSq(self))
+
+    def angle(self, v1):
+        "Return the angle in radians between self and argument vectors."
+        cosa = (self * v1) / (self.length() * v1.length())
+        return math.acos(cosa)
+
--- tests/test_vector3d.py
+++ tests/test_vector3d.py
@@ -2,8 +2,9 @@
 # tests for Vector3d
 
 import pytest
-from vector3d import Vector3d
+from vector3d import Vector3d, normalize
 import array as ar
+import math
 
 def test_init_nil():
     vnil = Vector3d()
@@ -22,3 +23,44 @@ def test_matmul():
     v2 = Vector3d(0.0, 1.0, 0.0)
     v3 = v1 @ v2
     assert v3.elems == ar.array('d', [0.0, 0.0, 1.0])
+
+def vectordiff(v1, v2):
+    v0 = Vector3d()
+    v0 = v1 - v2
+    s = v0*v0
+    assert isinstance(s, float)
+    return s
+
+def test_normalize_nonclass():
+    v1 = Vector3d(3.0, 4.0, 5.0)
+    v2 = Vector3d()
+    vnormx = Vector3d(0.424264068711928,0.565685424949238,0.7071067811865475)
+    v2 = normalize(v1)
+    assert vectordiff (v2, vnormx) < 1.0e-10
+
+def test_normalize():
+    v1 = Vector3d(3.0, 4.0, 5.0)
+    vnormx = Vector3d(0.424264068711928,0.565685424949238,0.7071067811865475)
+    v1.normalize()
+    assert vectordiff (v1, vnormx) < 1.0e-10
+
+def test_lengthSquared():
+    v1 = Vector3d(3.0, 4.0, 5.0)
+    lsq = v1.lengthSquared()
+    assert lsq == 50.0
+
+def test_length():
+    v1 = Vector3d(3.0, 4.0, 5.0)
+    dist = v1.length()
+    ref = 7.0710678118654755
+    assert (abs(dist-ref)) < 1.0e-10
+
+def test_angle():
+    v0 = Vector3d(1.0, 0.0, 0.0)
+    v1 = Vector3d(1.0, 1.0, 0.0)
+    ref = math.pi/4.0
+    a = v0.angle(v1)
+    assert (abs(a-ref)) < 1.0e-10
+    a = v1.angle(v0)
+    assert (abs(a-ref)) < 1.0e-10
+
